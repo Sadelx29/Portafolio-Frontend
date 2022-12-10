2b4d54197199b9bd47c4be06735d18319371b2ef
Sadelx29 <96783535+Sadelx29@users.noreply.github.com>
2022-12-10 09:08:20 -0400

    header completo, solo falta programar el boton

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Testimonials from './pages/Testimonials'
+import Projects from './pages/Projects'
 import { Routes,Route } from 'react-router-dom'
 
 
@@ -15,6 +16,7 @@ function App() {
       <Route path='/about' element={<About/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/testimonials' element={<Testimonials/>}/>
+      <Route path='/projects' element={<Projects/>}/>
     </Routes>
 
     </>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Header from '../components/Header.jsx'
+import Footer from '../components/Footer.jsx';
+
 
 const Projects = () => {
   return (
